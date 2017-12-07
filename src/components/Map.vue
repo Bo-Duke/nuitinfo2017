f3933377754453527f7db00c060b38900d5e42ad
Maxime Bouveron <maxime.bouveron@gmail.com>
2017-12-07 23:02:53 +0100

    Dark map theme is easier on the eyes for drunk people (well, this one is TOO dark, but you got the idea)

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,6 +3,7 @@
     class="gmap-map"
     :center="center"
     :zoom="13"
+    :options="mapStyle"
   >
     <gmap-marker
       :position="marker.position"
@@ -10,6 +11,8 @@
       :clickable="true"
       :draggable="false"
     ></gmap-marker>
+    <gmap-user-position
+  />
   </gmap-map>
 </template>
 
@@ -19,6 +22,38 @@ export default {
     return {
       center: { lat: 0, lng: 0 },
       marker: {},
+      mapStyle: {
+        styles: [
+          {
+            stylers: [
+              {
+                hue: '#ff1a00',
+              },
+              {
+                invert_lightness: true,
+              },
+              {
+                saturation: -100,
+              },
+              {
+                lightness: 33,
+              },
+              {
+                gamma: 0.5,
+              },
+            ],
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [
+              {
+                color: '#2D333C',
+              },
+            ],
+          },
+        ],
+      },
     };
   },
 
