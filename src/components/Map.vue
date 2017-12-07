5fa8febeca2b1a3642b0b8184ae53aa70a71b63a
Maxime Bouveron <maxime.bouveron@gmail.com>
2017-12-07 22:50:25 +0100

    Simple geolocation (with a little car icon, cute)

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,38 @@
 <template>
   <gmap-map
     class="gmap-map"
-    :center="{lat:43.6047, lng:1.4442}"
+    :center="center"
     :zoom="13"
-  ></gmap-map>
+  >
+    <gmap-marker
+      :position="marker.position"
+      :icon.sync="marker.icon"
+      :clickable="true"
+      :draggable="false"
+    ></gmap-marker>
+  </gmap-map>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      center: { lat: 0, lng: 0 },
+      marker: {},
+    };
+  },
+
+  mounted() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(data => {
+        this.marker = {
+          position: { lat: data.coords.latitude, lng: data.coords.longitude },
+          icon: require('../assets/position.png'),
+        };
+        this.center = this.marker.position;
+      });
+  },
+};
 </script>
 
 <style scoped>
