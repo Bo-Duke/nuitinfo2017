682beeb20851132ac1044aad12028ed98fca8882
Maxime Bouveron <maxime.bouveron@gmail.com>
2017-12-07 21:43:34 +0100

    Added a very simple component

---
+++ src/components/Map.vue
@@ -0,0 +1,18 @@
+<template>
+  <gmap-map
+    class="gmap-map"
+    :center="{lat:43.6047, lng:1.4442}"
+    :zoom="13"
+  ></gmap-map>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.gmap-map {
+  width: 100%;
+  height: 450px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import * as VueGoogleMaps from 'vue2-google-maps';
 import App from './App';
 import router from './router';
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAQr1TWMiJXF572V_hHO9sRvQDpEW2Jwq0',
+    libraries: 'places',
+  },
+});
 
 Vue.config.productionTip = false;
 
